66d4d3b41ec72385c678ccf156f75b4750e82079
ningbingjian <ningbingjian24>
2018-01-24 11:00:29 +0800

    add

---
+++ ning-util/ning-util-scala/src/main/scala/com/ning/util/date/DateUtils.scala
@@ -0,0 +1,15 @@
+package com.ning.util.date
+
+/**
+  *
+  * Created by ning on 2018/1/24.
+  * User:ning
+  * Date:2018/1/24
+  * tIME:10:51
+  *
+  */
+object DateUtils {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
