03c829b774b621cdabe8690c72cbaac3f52d0f65
ningbingjian1 <ningbingjian1@126.com>
2018-11-18 10:48:22 +0800

    add code and doc

--- ning-netty/ning-netty-gradle-grpc/src/main/java/io/grpc/examples/StudentClient.java
+++ ning-netty/ning-netty-gradle-grpc/src/main/java/io/grpc/examples/StudentClient.java
@@ -4,10 +4,23 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.examples.student.proto.StudentRequest;
 import io.grpc.examples.student.proto.StudentResponse;
+import io.grpc.examples.student.proto.StudentResponseList;
 import io.grpc.examples.student.proto.StudentServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 public class StudentClient {
     public static void main(String[] args)throws Exception {
+        //testGetByNames();
+        //testGetByAge();
+        //testGetByCity();
+        return;
+
+    }
+
+    private static void testGetByNames() throws IOException {
         ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",8899 )
                 .usePlaintext(true)
                 .build();
@@ -17,6 +30,46 @@ public class StudentClient {
                 studentServiceBlockingStub.getByName(StudentRequest.newBuilder().setName("NAME-client").build());
         System.out.println(response);
         System.in.read();
-
+    }
+    private static void testGetByAge()throws Exception{
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",8899 )
+                .usePlaintext(true)
+                .build();
+        StudentServiceGrpc.StudentServiceBlockingStub studentServiceBlockingStub = StudentServiceGrpc
+                .newBlockingStub(managedChannel);
+        Iterator<StudentResponse> responses =
+                studentServiceBlockingStub.getByAge(StudentRequest.newBuilder().setName("client-u1").setAge(1).build());
+        while(responses.hasNext()){
+            System.out.println(responses.next());
+        }
+        System.in.read();
+    }
+    public static void testGetByCity() throws Exception{
+        StreamObserver<StudentResponseList> responseObserver = new StreamObserver<StudentResponseList>() {
+            @Override
+            public void onNext(StudentResponseList value) {
+                value.getStudentResponseList().forEach(response -> {
+                    System.out.println(response);
+                });
+            }
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+        };
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",8899 )
+                .usePlaintext(true)
+                .build();
+        StudentServiceGrpc.StudentServiceStub stub = StudentServiceGrpc.newStub(managedChannel);
+        StreamObserver<StudentRequest> requestStreamObserver = stub.getByCity(responseObserver);
+        requestStreamObserver.onNext(StudentRequest.newBuilder().setName("u1").setCity("city1").build());
+        requestStreamObserver.onNext(StudentRequest.newBuilder().setName("u2").setCity("city2").build());
+        requestStreamObserver.onNext(StudentRequest.newBuilder().setName("u3").setCity("city3").build());
+        requestStreamObserver.onCompleted();
+        System.in.read();
     }
 }
--- ning-netty/ning-netty-gradle-grpc/src/main/java/io/grpc/examples/StudentServiceImpl.java
+++ ning-netty/ning-netty-gradle-grpc/src/main/java/io/grpc/examples/StudentServiceImpl.java
@@ -25,16 +25,42 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
     @Override
     public void getByAge(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
-        super.getByAge(request, responseObserver);
+        System.out.println(request.getName() + "," + request.getAge());
+        StudentResponse response1 = StudentResponse.newBuilder().setName("u1").build();
+        StudentResponse response2 = StudentResponse.newBuilder().setName("u2").build();
+        responseObserver.onNext(response1);
+        responseObserver.onNext(response2);
+        responseObserver.onCompleted();
     }
 
     @Override
     public StreamObserver<StudentRequest> getByCity(StreamObserver<StudentResponseList> responseObserver) {
-        return super.getByCity(responseObserver);
+        return new StreamObserver<StudentRequest>() {
+            @Override
+            public void onNext(StudentRequest value) {
+                System.out.println(" receive client request : " + value);
+            }
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("client error  : ");
+            }
+            @Override
+            public void onCompleted() {
+                System.out.println("client onCompleted");
+                StudentResponse response1 = StudentResponse.newBuilder().setName("u1").setAge(100).build();
+                StudentResponse response2 = StudentResponse.newBuilder().setName("u2").setAge(100).build();
+                StudentResponseList list = StudentResponseList.newBuilder()
+                        .addStudentResponse(response1)
+                        .addStudentResponse( response2)
+                        .build();
+                responseObserver.onNext(list);
+                responseObserver.onCompleted();
+            }
+        };
     }
 
     @Override
     public StreamObserver<StudentRequest> getByProvince(StreamObserver<StudentResponse> responseObserver) {
-        return super.getByProvince(responseObserver);
+
     }
 }
